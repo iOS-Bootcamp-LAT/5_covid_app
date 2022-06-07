2b392058e14fdbcc01a35300af629c3cda78fffa
David Granado J <david.granado@interfase.com.bo>
2022-06-07 09:10:43 -0400

    Initial Commit

---
+++ 5_covid_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5_covid_app
+//
+//  Created by David Granado Jordan on 6/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
